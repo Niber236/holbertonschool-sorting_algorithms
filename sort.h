e81656d5bbaab5fdb2380b7185e77f4e1faa0e98
Niber236 <berniskouzou45@gmail.com>
2025-12-10 13:46:33 +0000

    task1

--- sort.h
+++ sort.h
@@ -16,8 +16,13 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/* Fonctions d'affichage (fournies) */
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+ /* pour les task */
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif
